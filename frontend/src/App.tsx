2810e16f5dd8e0e88b2e5b40b9c82caa4cffbe4d
Tejack98 <choudhari.t@northeastern.edu>
2024-10-01 19:58:58 -0400

    Added Search Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import Card from './Components/Card/Card';
 import CardList from './Components/CardList/CardList';
+import Search from './Components/Search/Search';
 
 function App() {
   return (
     <div className="App">
+      <Search/>
       <CardList />
     </div>
   );
---
+++ frontend/src/Components/Search/Search.tsx
@@ -0,0 +1,24 @@
+import React, { ChangeEvent, useState, SyntheticEvent } from 'react'
+
+type Props = {}
+
+const Search : React.FC<Props> = (props: Props): JSX.Element => {
+
+    const [search, setSearch] = useState<string>("");
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value);
+        console.log(e.target.value);
+    }
+
+    const onClick = (e: SyntheticEvent) => {console.log(e)};
+
+  return (
+    <div>
+        <input type="text" value={search} onChange={(e) => handleChange(e)} />
+        <button onClick={(e)=>onClick(e)}>Search</button>
+    </div>
+  )
+}
+
+export default Search;

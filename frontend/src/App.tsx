15e5da84707248c76ce4eee80142d7d1771236f3
Tejack98 <choudhari.t@northeastern.edu>
2024-10-07 19:18:51 -0400

    Added React Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,77 +1,12 @@
-import React, { ChangeEvent, SyntheticEvent, useState } from 'react';
-import logo from './logo.svg';
+import { Outlet } from 'react-router';
 import './App.css';
-import Card from './Components/Card/Card';
-import CardList from './Components/CardList/CardList';
-import Search from './Components/Search/Search';
-import { CompanySearch } from './company';
-import { searchCompanies } from './api';
-import { on } from 'events';
-import ListPortfolio from './Components/Portfolio/ListPortfolio/ListPortfolio';
 import Navbar from './Components/Navbar/Navbar';
-import Hero from './Components/Hero/Hero';
 
 function App() {
-  const [search, setSearch] = useState<string>("");
-  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
-  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
-  const [serverError, setServerError] = useState<string | null>(null);
-    
-  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-      setSearch(e.target.value);
-  }
-
-  const onClick = async (e: SyntheticEvent) => {
-    const result = await searchCompanies(search);
-    if(typeof result === "string"){
-      setServerError(result);
-    }
-    else if (Array.isArray(result.data)){
-      setSearchResult(result.data);
-    }
-
-    console.log(searchResult);
-    
-  };
-
-  const onPortfolioCreate = (e: any) => {
-    e.preventDefault();
-    const exits = portfolioValues.find((value) => value === e.target[0].value);
-    if(exits)return;
-    const updatedPortfolio = [...portfolioValues, e.target[0].value];
-    setPortfolioValues(updatedPortfolio);
-  }
-
-  const onPortfolioDelete = (e: any) => {
-    e.preventDefault();
-    const updatedPortfolio = portfolioValues.filter((value) => value !== e.target[0].value);
-    setPortfolioValues(updatedPortfolio);
-  }
-
-  const onSearchSubmit = async (e: SyntheticEvent) => {
-    e.preventDefault();
-    const result = await searchCompanies(search);
-    //setServerError(result.data);
-    if (typeof result === "string") {
-      setServerError(result);
-    } else if (Array.isArray(result.data)) {
-      setSearchResult(result.data);
-    }
-  };
-
-  return (
-    <div className="App">
-      <Navbar />
-      {/* <Hero />  */}
-      <Search  
-        onSearchSubmit={onSearchSubmit}
-        search={search} 
-        handleSearchChange={handleSearchChange}/>
-      <ListPortfolio portfolioValues={portfolioValues} onPortfolioDelete={onPortfolioDelete}/>
-      <CardList searchResults={searchResult}  onPortfolioCreate={onPortfolioCreate}/>
-      {serverError && <h1>{serverError}</h1>}
-    </div>
-  );
+  return (<>
+    <Navbar />
+    <Outlet />
+  </>);
 }
 
 export default App;
--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -2,6 +2,7 @@ import React, { SyntheticEvent } from 'react';
 import './Card.css';
 import { CompanySearch } from '../../company';
 import AddPortfolio from '../Portfolio/AddPortfolio/AddPortfolio';
+import { Link } from 'react-router-dom';
 
 interface Props  {
   id: string;
@@ -16,9 +17,9 @@ const Card: React.FC<Props> = ({id, searchResult, onPortfolioCreate}: Props): JS
       key={id}
       id={id}
     >
-      <h2 className="font-bold text-center text-Black md:text-left">
+      <Link to={`/company/${searchResult.symbol}`} className="font-bold text-center text-Black md:text-left">
         {searchResult.name} ({searchResult.symbol})
-      </h2>
+      </Link>
       <p className="text-Black">{searchResult.currency}</p>
       <p className="font-bold text-Black">
         {searchResult.exchangeShortName} - {searchResult.stockExchange}

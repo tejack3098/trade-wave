bafa97184af98031b1f9f33431f0423a15d9652f
Tejack98 <choudhari.t@northeastern.edu>
2024-10-08 14:15:20 -0400

    updated Design Guide and Added RatioList

--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -5,7 +5,7 @@ import SearchPage from "../Pages/SearchPage/SearchPage";
 import CompanyPage from "../Pages/CompanyPage/CompanyPage";
 import CompanyProfile from "../Components/CompanyProfile/CompanyProfile";
 import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
-import DesignPage from "../Pages/DesignPage/DesignPage";
+import DesignGuide from "../Pages/DesignGuide/DesignGuide";
 
 export const router = createBrowserRouter([
     {
@@ -22,7 +22,7 @@ export const router = createBrowserRouter([
             },
             {
                 path: "design-guide",
-                element: <DesignPage />
+                element: <DesignGuide />
             },
             {
                 path: "company/:ticker",
--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { CompanyProfile, CompanySearch } from "./company"
+import { CompanyProfile, CompanySearch, CompanyKeyMetrics } from "./company"
 
 interface SearchResponse {
     data: CompanySearch[]
@@ -33,5 +33,17 @@ export const getCompanyProfile = async (query: string) => {
     } 
     catch (error) {
        console.log(error)
+    }   
+}
+
+export const getKeyMetrics = async (query: string) => {
+    try {
+        const data = await axios.get<CompanyKeyMetrics[]>(
+            `https://financialmodelingprep.com/api/v3/key-metrics-ttm/${query}?apikey=${process.env.REACT_APP_API_KEY}`
+        );
+        return data;
+    } 
+    catch (error: any) {
+       console.log("error message: ", error.message);
     }   
 }

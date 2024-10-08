c48e494597b931f02133483d4a281f4a74dad5ec
Tejack98 <choudhari.t@northeastern.edu>
2024-10-08 08:27:34 -0400

    Env Delete

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { getCompanyProfile } from '../../api';
+import { CompanyProfile } from '../../company';
+import { get } from 'http';
 
 type Props = {}
 
 const CompanyPage = (props: Props) => {
-  return (
-    <div>CompanyPage</div>
-  )
+
+  let { ticker } = useParams();
+  const [company, setCompany] = useState<CompanyProfile>();
+
+  useEffect(() => {
+    const getProfileInit = async () => {
+      const result = await getCompanyProfile(ticker!);
+      setCompany(result?.data[0]);
+    }
+    getProfileInit();
+  },[])
+
+  return <>
+    {
+      company ? (
+        <div>{company.companyName}</div>
+      ):(
+        <div>Company Not Found!</div>
+      )}
+  </>
 }
 
 export default CompanyPage

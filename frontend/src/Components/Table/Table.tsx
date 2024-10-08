eaf26781589c6c2876430865424a40a0d5bfcdfa
Tejack98 <choudhari.t@northeastern.edu>
2024-10-08 15:08:22 -0400

    Added Income Statement

--- frontend/src/Components/Table/Table.tsx
+++ frontend/src/Components/Table/Table.tsx
@@ -1,39 +1,28 @@
 import React from 'react'
 import { testIncomeStatementData } from './testData';
 
-const data = testIncomeStatementData;
+type Props = {
+    config: any,
+    data: any
+}
 
-type Props = {}
-
-type Company = (typeof data)[0];
-
-const configs = [
-    {
-        label: "Year",
-        render: (company: Company) => company.acceptedDate
-    },
-    {
-        label: "Cost of Revenue",
-        render: (company: Company) => company.costOfRevenue
-    },
-];
-
-const Table = (props: Props) => {
-  const renderedRows = data.map((company) => {
+const Table = ({config, data}: Props) => {
+  const renderedRows = data.map((company: any) => {
     return (
       <tr key={company.cik}>
-        {configs.map((val: any) => {
-          return (
-            <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
-              {val.render(company)}
-            </td>
-          );
+         {
+            config.map((val: any) => {
+                return (
+                    <td className='p-3 text-left text-xs font-medium text-fray-500 uppercase tracking-wider'>
+                        {val.render(company)}
+                    </td>
+                )
         })}
       </tr>
     );
   });
 
-  const renderedHeaders = configs.map((config: any) => {
+  const renderedHeaders = config.map((config: any) => {
     return (
         <th className='p-4 text-left text-xs font-medium text-fray-500 uppercase tracking-wider'
         key={config.label}>
--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { CompanyProfile, CompanySearch, CompanyKeyMetrics } from "./company"
+import { CompanyProfile, CompanySearch, CompanyKeyMetrics, CompanyIncomeStatement } from "./company"
 
 interface SearchResponse {
     data: CompanySearch[]
@@ -40,6 +40,18 @@ export const getKeyMetrics = async (query: string) => {
     try {
         const data = await axios.get<CompanyKeyMetrics[]>(
             `https://financialmodelingprep.com/api/v3/key-metrics-ttm/${query}?apikey=${process.env.REACT_APP_API_KEY}`
+        );
+        return data;
+    } 
+    catch (error: any) {
+       console.log("error message: ", error.message);
+    }   
+}
+
+export const getIncomeStatement = async (query: string) => {
+    try {
+        const data = await axios.get<CompanyIncomeStatement[]>(
+            `https://financialmodelingprep.com/api/v3/income-statement/${query}?limit=40&apikey=${process.env.REACT_APP_API_KEY}`
         );
         return data;
     } 

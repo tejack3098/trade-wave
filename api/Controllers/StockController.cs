9e4fcd546c586b4ffd024f91f1bbbbde7e267801
Tejas Choudhari <tejas.choudhari36@gmail.com>
2024-10-12 18:10:23 -0400

    added async await

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -7,6 +7,7 @@ using api.Data;
 using api.Dtos.Stock;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace api.Controllers
@@ -24,18 +25,18 @@ namespace api.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            var stocks = _context.Stocks.ToList()
-                .Select(stock => stock.ToStockDto());;
+            var stocks = await  _context.Stocks.ToListAsync();
+            var stocksDto = stocks.Select(stock => stock.ToStockDto());
             return Ok(stocks);
         }
 
 
         [HttpGet("{id}")]
-        public IActionResult GetById([FromRoute]int id)
+        public async Task<IActionResult> GetById([FromRoute]int id)
         {
-            var stock = _context.Stocks.Find(id);
+            var stock = await _context.Stocks.FindAsync(id);
                 
             if (stock == null)
             {
@@ -46,18 +47,18 @@ namespace api.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create([FromBody]CreateStockRequestDto stockDto)
+        public async Task<IActionResult> Create([FromBody]CreateStockRequestDto stockDto)
         {
             var stockModel = stockDto.ToStockFromCreateDto();
-            _context.Stocks.Add(stockModel);
-            _context.SaveChanges();
+            await _context.Stocks.AddAsync(stockModel);
+            await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
         }
 
         [HttpPut("{id}")]
-        public IActionResult Update([FromRoute]int id, [FromBody]UpdateStockRequestDto updateStockDto)
+        public async Task<IActionResult> Update([FromRoute]int id, [FromBody]UpdateStockRequestDto updateStockDto)
         {
-            var stockModel = _context.Stocks.FirstOrDefault(s => s.Id == id);
+            var stockModel = await _context.Stocks.FirstOrDefaultAsync(s => s.Id == id);
             
             if (stockModel == null)
             {
@@ -70,20 +71,20 @@ namespace api.Controllers
             stockModel.Purchase = updateStockDto.Purchase;
             stockModel.Symbol =  updateStockDto.Symbol;
             stockModel.CompanyName = updateStockDto.CompanyName;
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
             return Ok(stockModel.ToStockDto());
         }
 
         [HttpDelete("{id}")]
-        public IActionResult Delete([FromRoute]int id)
+        public async Task<IActionResult> Delete([FromRoute]int id)
         {
-            var stockModel = _context.Stocks.FirstOrDefault(s => s.Id == id);
+            var stockModel = await _context.Stocks.FirstOrDefaultAsync(s => s.Id == id);
             if (stockModel == null)
             {
                 return NotFound();
             }
             _context.Stocks.Remove(stockModel);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
             return NoContent();
         }
     }
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b3bfd5d23be2caf2a0074d3e1046ebb156ac4eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6197ec79f7772ea493fdfe9239fee44c760820e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

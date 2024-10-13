9f21fb05e64978c59e2bc944feca03eb31e9d899
Tejas Choudhari <tejas.choudhari36@gmail.com>
2024-10-13 17:21:22 -0400

    Register User Account Controller added

---
+++ api/Migrations/20241013211518_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "96325c36-38b0-46bd-a20b-84f171d1fc02", null, "Admin", "ADMIN" },
+                    { "e090ba6d-a219-448e-a157-d8fb62b7540a", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "96325c36-38b0-46bd-a20b-84f171d1fc02");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e090ba6d-a219-448e-a157-d8fb62b7540a");
+        }
+    }
+}

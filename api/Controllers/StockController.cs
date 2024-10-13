ef312d43d8e1a6705fed38e2871b132615f238be
Tejas Choudhari <tejas.choudhari36@gmail.com>
2024-10-13 19:49:12 -0400

    added Swagger Authorize

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -8,6 +8,7 @@ using api.Dtos.Stock;
 using api.Helpers;
 using api.Interfaces;
 using api.Mappers;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -30,6 +31,7 @@ namespace api.Controllers
         }
 
         [HttpGet]
+        [Authorize]
         public async Task<IActionResult> GetAll([FromQuery] QueryObject query)
         {
             if (!ModelState.IsValid)

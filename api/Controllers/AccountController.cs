78685afe8043cb8a9cd54d953516b1eb39fdf72b
Tejas Choudhari <tejas.choudhari36@gmail.com>
2024-10-13 19:39:18 -0400

    Added Account Controller

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -17,11 +18,47 @@ namespace api.Controllers
         private readonly UserManager<AppUser> _userManager;
 
         private readonly ITokenService _tokenService;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody]LoginDto loginDto)
+        {
+            try
+            {
+                if(!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.UserName);
+
+                if(user == null)
+                    return Unauthorized("Invalid Username!");
+
+                var result = await _signInManager
+                    .CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+                if(!result.Succeeded)
+                    return Unauthorized("Username not found or Invalid Password!");
+
+                return Ok(
+                    new NewUserDto
+                    {
+                        UserName = user.UserName, 
+                        Email = user.Email,
+                        Token = _tokenService.CreateToken(user)
+                    }
+                );
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         [HttpPost("register")]
---
+++ api/Dtos/Account/LoginDto.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace api.Dtos.Account
+{
+    public class LoginDto
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
